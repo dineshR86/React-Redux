842b59cd55e77c39b99fc6ba168cc369810e81b8
dineshr86 <dinesh_gudapati@outlook.com>
2018-07-04 10:53:42 +0530

    Included build

--- src/components/app.js
+++ src/components/app.js
@@ -17,7 +17,7 @@ export default class App extends Component {
       selectedVideo:null
     };
     
-    this.videoSearch("surfboard");
+    this.videoSearch("chelsea");
   }
 
   videoSearch(term){
